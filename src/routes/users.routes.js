64d85be95648eaf66704e4adaee70bc201437e07
denilsonbaptista <baptistadenilsonbp@gmail.com>
2024-03-22 10:52:07 -0300

    refactor

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,9 @@
 const { Router } = require('express')
 
 const UserController = require('../controllers/UserController')
+const userController = new UserController()
 
 const usersRoutes = Router()
-
-const userController = new UserController()
 
 usersRoutes.post('/', userController.createUser)
 usersRoutes.put('/:id', userController.updatedUser)
